07e617583ea1cc6ba9136ef383503886595679e5
Alp Eren Velioğlu <67262963+alpvelioglu@users.noreply.github.com>
2025-02-22 18:44:53 +0300

    error fixed

--- src/Application/EventManagement.Application/Events/Commands/CreateEvent/CreateEventCommand.cs
+++ src/Application/EventManagement.Application/Events/Commands/CreateEvent/CreateEventCommand.cs
@@ -1,9 +1,12 @@
+using EventManagement.Application.Common.Mappings;
+using EventManagement.Core.Entities;
 using EventManagement.Core.Common;
 using MediatR;
+using AutoMapper;
 
 namespace EventManagement.Application.Events.Commands.CreateEvent;
 
-public record CreateEventCommand : IRequest<Result<int>>
+public record CreateEventCommand : IRequest<Result<int>>, IMapFrom<Event>
 {
     public string Name { get; init; } = string.Empty;
     public string Description { get; init; } = string.Empty;
@@ -12,4 +15,19 @@ public record CreateEventCommand : IRequest<Result<int>>
     public int MaxParticipants { get; init; }
     public int OrganizerId { get; init; }
     public int VenueId { get; init; }
+
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<CreateEventCommand, Event>()
+            .ForMember(d => d.Id, opt => opt.Ignore())
+            .ForMember(d => d.Status, opt => opt.Ignore())
+            .ForMember(d => d.CreatedAt, opt => opt.Ignore())
+            .ForMember(d => d.UpdatedAt, opt => opt.Ignore())
+            .ForMember(d => d.IsDeleted, opt => opt.Ignore())
+            .ForMember(d => d.CurrentParticipants, opt => opt.Ignore())
+            .ForMember(d => d.Organizer, opt => opt.Ignore())
+            .ForMember(d => d.Venue, opt => opt.Ignore())
+            .ForMember(d => d.Participants, opt => opt.Ignore())
+            .ForMember(d => d.RegisteredUsers, opt => opt.Ignore());
+    }
 }
